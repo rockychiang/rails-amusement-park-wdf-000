bac4ca2200ab9ba20dcf85411b9ce594321c6d50
rockychiang <rocky@chem.ucla.edu>
2018-04-22 20:15:05 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,37 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    error =[]
+    error << check_ticket
+    error << check_height
+
+    if error.compact.empty?
+      update_user
+    else
+      "Sorry. " + error.compact.join(" ")
+    end
+  end
+
+  private
+
+  def check_ticket
+    if user.tickets < attraction.tickets
+      "You do not have enough tickets to ride the #{attraction.name}."
+    end
+  end
+
+  def check_height
+    if user.height < attraction.min_height
+      "You are not tall enough to ride the #{attraction.name}."
+    end
+  end
+
+  def update_user
+    user.tickets -= attraction.tickets
+    user.happiness += attraction.happiness_rating
+    user.nausea += attraction.nausea_rating
+    user.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+    happiness > nausea ? "happy" : "sad"
+  end
+
 end
